20404e083e5bf7a1a0c71c979a190fc10924653f
Buck417 <buck417@gmail.com>
2018-03-08 08:21:32 -0700

    Working on image processing. Nothing works perfectly for now.

--- app/src/main/java/com/example/android/visolver/Result.java
+++ app/src/main/java/com/example/android/visolver/Result.java
@@ -9,7 +9,19 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
 import java.io.File;
+import java.util.ArrayList;
 
 public class Result extends AppCompatActivity {
 
@@ -17,8 +29,28 @@ public class Result extends AppCompatActivity {
 
     TextView resultView;
     ImageView previewImage;
+    private Bitmap originalImage;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i("OpenCV", "OpenCV loaded successfully");
+                    buildMat(originalImage);
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
@@ -32,9 +64,43 @@ public class Result extends AppCompatActivity {
             resultView.setText(results);
             File myPic = new File(myIntent.getStringExtra("BITMAP"));
             if(myPic.exists()){
-                Bitmap myBitmap = BitmapFactory.decodeFile(myPic.getAbsolutePath());
-                previewImage.setImageBitmap(myBitmap);
+                originalImage = BitmapFactory.decodeFile(myPic.getAbsolutePath());
             }
         }
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.d("OpenCV", "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    private void buildMat(Bitmap bmp){
+        Mat myImg = new Mat();
+        Utils.bitmapToMat(bmp, myImg);
+        Mat grayImg = new Mat();
+        Imgproc.cvtColor(myImg, grayImg, Imgproc.COLOR_RGB2GRAY);
+        org.opencv.core.Size s = new Size(5, 5);
+        Mat blurImg = new Mat();
+        Imgproc.GaussianBlur(grayImg, blurImg, s, 5, 5);
+        Mat threshImg = new Mat();
+        Imgproc.adaptiveThreshold(blurImg, threshImg, 255, 1, 1, 11, 2);
+
+        ArrayList<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(threshImg, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0,0));
+        Mat result = new Mat();
+        //Mat ans = new Mat(threshImg.size(), )
+        Scalar color = new Scalar(50, 50, 50);
+        Imgproc.drawContours(result, contours, 0, color);
+        Utils.matToBitmap(result, bmp);
+        previewImage.setImageBitmap(bmp);
     }
 }
